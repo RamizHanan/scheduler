7c083f95ba61ff2f32d0d24b60d61a1e51dd7b8e
RigobertoM29 <rigomacedo0629@gmail.com>
2019-11-05 18:43:42 -0800

    renamed main, created scheduler class

--- scheduler.py
+++ scheduler.py
@@ -1,41 +1,40 @@
-import argparse
-from task import Task
+class Scheduler(object):
 
+    def __init__(self, task_count: int, exec_time: int, ap1188: int, ap918: int,
+                 ap648: int, ap384: int, apidle: int, sch_type: str, ee: bool):
+        self.task_count = task_count
+        self.exec_time = exec_time
+        self.ap1188 = ap1188
+        self.ap918 = ap918
+        self.ap648 = ap648
+        self.ap384 = ap384
+        self.apidle = apidle
+        self.sch_type = sch_type
+        self.ee = ee
 
-def parse_cmd_args():
-    parser = argparse.ArgumentParser(description='Create schedule using EDF or other')
-    parser.add_argument('file', type=str, nargs='?', help='File containing jobs to schedule')
-    parser.add_argument('scheduler', type=str, nargs='?', help='Type of scheduler', choices=['EDF', 'edf', 'RM', 'rm'])
-    parser.add_argument('EE', type=str, nargs='?', default='', choices=['EE', 'ee', ''], help='Energy Efficient')
-    args = parser.parse_args()
-    return args
+    def schedule(self):
+        if self.sch_type.lower() is 'edf' and self.ee is False:
+            self.EDF()
+        elif self.sch_type.lower() is 'rm' and self.ee is False:
+            self.RM()
+        elif self.sch_type.lower() is 'edf' and self.ee is True:
+            self.EDF_EE()
+        elif self.sch_type.lower() is 'rm' and self.ee is True:
+            self.RM_EE()
 
+    def EDF(self):
+        pass
 
-def create_tasks(file_name):
-    tasks = []
-    with open(file_name, 'r') as f:
-        next(f)
-        for line in f:
-            param = line.split()
-            tasks.append(Task(*param))
+    def RM(self):
+        pass
 
-    for task in tasks:
-        print(task)
-    return tasks
+    def EDF_EE(self):
+        pass
 
+    def RM_EE(self):
+        pass
 
-def main():
-    args = parse_cmd_args()
-
-    file_name = args.file
-    schedule_type = args.scheduler
-    EE_enable = False if args.EE is '' else True
-
-    print('SCH: {}'.format(schedule_type))
-    print('EE: {}'.format(EE_enable))
-
-    tasks = create_tasks(file_name)
-
-
-if __name__ == '__main__':
-    main()
+    def __str__(self):
+        return "Scheduler: {} {} {} {} {} {} {} ({} {})".format(self.task_count, self.exec_time, self.ap1188,
+                                                                self.ap918, self.ap648, self.ap384, self.apidle,
+                                                                self.sch_type, self.ee)
--- task.py
+++ task.py
@@ -1,11 +1,12 @@
 class Task(object):
-    def __init__(self, name: str, deadline: int, ap1188: int, ap918: int, ap648: int, ap384: int):
+
+    def __init__(self, name: str, deadline: int, wcet1188: int, wcet918: int, wcet648: int, wcet384: int):
         self.name = name
         self.deadline = deadline
-        self.ap1188 = ap1188
-        self.ap918 = ap918
-        self.ap648 = ap648
-        self.ap384 = ap384
+        self.wcet1188 = wcet1188
+        self.wcet918 = wcet918
+        self.wcet648 = wcet648
+        self.wcet384 = wcet384
 
     def __str__(self):
-        return "Task: {} {} {} {} {} {}".format(self.name, self.deadline, self.ap1188, self.ap918, self.ap648, self.ap384)
+        return "Task: {} {} {} {} {} {}".format(self.name, self.deadline, self.wcet1188, self.wcet918, self.wcet648, self.wcet384)
