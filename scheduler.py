9d9d43ec43a2cceab8ebac13a57611b27aaf06f6
ramiz hanan <ramizhanan@gmail.com>
2019-11-12 23:28:11 -0800

    EDF attempt

--- scheduler.py
+++ scheduler.py
@@ -24,16 +24,15 @@ class Scheduler(object):
         elif self.sch_type.lower() == 'rm' and self.ee is True:
             self.RM_EE()
 
-    def getNextTask(self, deadlineList, currentTime,deadlineIteration, distanceTillDeadline, nextDeadline):
-        for task in deadlineList: #loop over all tasks
-            #move to execution function
-            if currentTime == int(nextDeadline[task]): #if time has reached a deadline: 
-                #self note: execution time should be 0 already
-                nextDeadline[task] = int(deadlineIteration[task]) * int(deadlineList[task]) #update its next deadline
-                deadlineIteration[task] += 1 #increment next deadline multiplier for that task
 
-            #create time to next deadline for each task
-            distanceTillDeadline[task] = int(nextDeadline[task]) - int(currentTime) 
+    def getNextTask(self, deadlineList, time, distanceTillDeadline, nextDeadline, readyList):
+        
+        if(self.idleCheck(readyList, time, nextDeadline) == True):
+            return 'IDLE'
+
+        for task in deadlineList: #loop over all tasks
+            #update distance until deadline
+            distanceTillDeadline[task] = int(nextDeadline[task]) - int(time) 
 
         # Using all() + list comprehension 
         # Finding min value (deadline) in dict
@@ -43,23 +42,51 @@ class Scheduler(object):
 
         return str(next[0])
 
+    def idleCheck(self, readyList, time, nextDeadline):
+        idle = False
+                
+        #if all execution times are 0 and there is time left until all their deadlines
+        if (all(executeTime == 0 for executeTime in readyList.values()) and 
+            all(deadline > time for deadline in nextDeadline.value())):
+            idle = True
 
-    # def executeTask(self,readyList, task):
-    #     readyList[task] -= 1
+        return idle
 
+    def checkExecutionFinished(self,readyList, task, time, executionTimes,
+                    deadlineIteration, nextDeadline, returnTime, deadlineList):
+        #next return time is the next deadline
+        if readyList[task] == 0 and time == int(returnTime[task]):
+            returnTime[task] = int(nextDeadline[task])#next return time is the next deadline
+            readyList[task] = executionTimes[task] #reset execution
+            nextDeadline[task] = int(deadlineIteration[task]) * int(deadlineList[task]) #update its next deadline
+            deadlineIteration[task] += 1 #increment next deadline multiplier for that task
 
-    #     pass
+    def executeTask(self,readyList, task, time, executionTimes,
+                    deadlineIteration, nextDeadline, returnTime, deadlineList):
+        if task == "IDLE":
+            return task
+
+        #execute
+        readyList[task] -= 1
+        executed = task
+
+        self.checkExecutionFinished(readyList, task, time, executionTimes,
+                                    deadlineIteration, nextDeadline, returnTime, deadlineList)
+        
+        return executed
 
 
 # name: str, deadline: int, wcet1188: int, wcet918: int, wcet648: int, wcet384: int
     def EDF(self, tasks) -> list:
         readyList = {} #list of remaining execution times
+        executionTimes = {} #constant list of execution times
         deadlineList = {} #constant list of deadlines
         deadlineIteration = {} #list of deadline iterations
         distanceTillDeadline = {} #which task got next
-        nextDeadline = {} 
+        nextDeadline = {}
+        returnTime = {} 
         edf = []
-        currentTime = 1
+        time = 1
         totalTime = int(self.exec_time) + 1
                                          
         #Check utilization
@@ -68,7 +95,7 @@ class Scheduler(object):
             execution = tasks[taskNum].wcet1188
             deadline = tasks[taskNum].deadline
             utilization += float(execution) / float(deadline)    
-        print("util amount {}".format(utilization))
+        #print("util amount {}".format(utilization))
 
         if utilization > 1.0:
             print('Utilization error!')
@@ -79,23 +106,21 @@ class Scheduler(object):
             readyList[tasks[i].name] = int(tasks[i].wcet1188) #initialize execution times
             deadlineList[tasks[i].name] = int(tasks[i].deadline) #initialize deadlines
             nextDeadline[tasks[i].name] = int(tasks[i].deadline) #duplicate of deadline list initially
+            returnTime[tasks[i].name] = int(tasks[i].deadline) #list for return times to the system
             deadlineIteration[tasks[i].name] = 2 #next deadline is 2 * initial deadline...then 3 .. 4
             distanceTillDeadline[tasks[i].name] = 0 #used to determine who got next
         
 
-        #for time in range(1, int(totalTime)):
-        next = self.getNextTask(deadlineList, currentTime, deadlineIteration, distanceTillDeadline, nextDeadline)
-        #execute(next)
-        currentTime += 1
+        for time in range(1, int(totalTime)):
+            nextTask = self.getNextTask(deadlineList, time, distanceTillDeadline, nextDeadline, readyList)
+            executeTask = self.executeTask(readyList, nextTask, time, executionTimes,
+                    deadlineIteration, nextDeadline, returnTime, deadlineList)
+            
+            edf.append(executeTask)
+            time += 1
 
-        edf.append(next)
-
-        #deadline table
-        #always take min of deadline table
-        #each step update execution left 
-
-
-        pass
+        print(str(edf))
+        return edf
 
     def RM(self, tasks):
         time_units = [None] * self.exec_time
